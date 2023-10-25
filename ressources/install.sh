af4bb4c3cdf93d2bf96d625f1e5954b85b634f1a
pifou25 <nicolas_gras@hotmail.com>
2023-10-25 15:21:20 +0200

    fix: remove php5 installation

--- ressources/install.sh
+++ ressources/install.sh
@@ -2,22 +2,7 @@
 touch /tmp/dependancy_monitoring_in_progress
 echo 0 > /tmp/dependancy_monitoring_in_progress
 versionPHP=$(php -r \@phpinfo\(\)\; | grep 'PHP Version' -m 1 | awk '{ print $4 }' | cut -d'.' -f1)
-if [ "$versionPHP" -eq 5 ]
-	then
-		echo "*****************************************************************************************************"
-		echo "*   Lancement de l'installation du module SSH pour PHP 5 / Launch install of module ssh2 for PHP 5  *"
-		echo "*****************************************************************************************************"
-		sudo apt-get clean
-		echo 20 > /tmp/dependancy_monitoring_in_progress
-		sudo apt-get update
-		echo 80 > /tmp/dependancy_monitoring_in_progress
-		sudo apt-get install -y php5-ssh2
-		echo 100 > /tmp/dependancy_monitoring_in_progress
-		echo "Tout est installé avec succès - Everything is successfully installed!"
-		echo "*****************************************************************************************************"
-		echo "*  Pour finaliser l'installation, redémarrer Jeedom / To complete the installation, restart Jeedom  *"
-		echo "*****************************************************************************************************"
-fi
+
 if [ "$versionPHP" -ge 7 ]
 	then
 		echo "*****************************************************************************************************"
@@ -34,9 +19,9 @@ if [ "$versionPHP" -ge 7 ]
 		echo "*  Pour finaliser l'installation, redémarrer Jeedom / To complete the installation, restart Jeedom  *"
 		echo "*****************************************************************************************************"
 fi
-if [ "$versionPHP" -ne 5 ] && [ "$versionPHP" -ne 7 ]
+if [ "$versionPHP" -ne 7 ]
 	then
-		echo "Installation KO, PHP5 ou PHP7 n'est pas installé"
+		echo "Installation KO, PHP7 n'est pas installé"
 		echo 100 > /tmp/dependancy_monitoring_in_progress
 fi
 rm /tmp/dependancy_monitoring_in_progress
